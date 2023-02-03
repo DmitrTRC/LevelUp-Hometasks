69b7bbe115e569ae39420f8ebb1d32a5d94a0508
Dmitry Morozov <9984398@gmail.com>
2023-02-03 21:11:52 +0300

    Task2 : Done Run : Ok Tests : None

--- Lesson2/ISolution.cs
+++ Lesson2/ISolution.cs
@@ -3,7 +3,6 @@ namespace Lesson2;
 public interface ISolution
 {
     // Run method
-    void Task();
-    void Run();
 
+    void Run();
 }
---
+++ Lesson2/Task2.cs
@@ -0,0 +1,70 @@
+namespace Lesson2;
+
+public class Task2 : ISolution
+{
+    class User
+    {
+        public User(string name, string surname, int age, string hobby)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Hobby = hobby;
+        }
+
+        public string Name { get; init; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Hobby { get; set; }
+
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Surname: {Surname}, Age: {Age}, Hobby: {Hobby}");
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Surname: {Surname}, Age: {Age}, Hobby: {Hobby}";
+        }
+    }
+
+    public void Run()
+    {
+        Console.Write("Name: ");
+        string? userName = Console.ReadLine() ?? string.Empty;
+
+        Console.Write("Surname: ");
+        string userSurname = Console.ReadLine() ?? string.Empty;
+
+        Console.Write("Age: ");
+        int userAge;
+
+        while (int.TryParse(Console.ReadLine(), out userAge) == false)
+        {
+            Console.Write("Incorrect input. Try again -> ");
+        }
+
+
+        Console.Write("Hobby: ");
+        string userHobby = Console.ReadLine() ?? string.Empty;
+
+
+        if (string.IsNullOrEmpty(userName))
+        {
+            Console.WriteLine("Name is null");
+        }
+        else
+        {
+            var user = new User(userName, userSurname, userAge, userHobby);
+
+            Console.WriteLine("New user created: \n");
+
+            Console.WriteLine("Using PrintInfo() method:");
+            user.PrintInfo();
+
+            Console.WriteLine("Using ToString() method:");
+            Console.WriteLine(user);
+        }
+    }
+}
