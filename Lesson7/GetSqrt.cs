ced573ef6a5f7e96303aca3686429fcc92709e0b
Dmitry Morozov <9984398@gmail.com>
2023-03-02 01:41:53 +0300

    Lesson 7
    Initial Commit

---
+++ Lesson7/GetSqrt.cs
@@ -0,0 +1,16 @@
+namespace Lesson7;
+
+/// Create console application which would be able to calculate square roots for the numbers in range from M to N, where M and N – arguments what can be passed by the user. Print the pairs “number, square root” in the ascending order.
+public class GetSqrt
+{
+    
+    //CONSTRUCTOR FROM M , N
+    public GetSqrt(int m, int n)
+    {
+        this.M = m;
+        this.N = n;
+    }
+    
+    
+    
+}
