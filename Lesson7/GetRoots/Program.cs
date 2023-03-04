49bb4519105febb21d2fe60febe2ee18aaf053b5
Dmitry Morozov <9984398@gmail.com>
2023-03-04 21:28:38 +0300

    --> Structure update

---
+++ Lesson7/GetRoots/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Concurrent;
+
+void PrintRootsRange(int min, int max)
+{
+    ConcurrentDictionary<int, double> roots = new ConcurrentDictionary<int, double>();
+
+    Parallel.For(min, max, (i) => { roots.TryAdd(i, Math.Sqrt(i)); });
+
+    foreach (var root in roots)
+    {
+        System.Console.WriteLine($"{root.Key} : {root.Value}");
+    }
+}
+
+PrintRootsRange(2, 100);
