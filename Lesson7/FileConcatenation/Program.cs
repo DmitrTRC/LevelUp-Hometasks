e063ccdab65b86f7b820ba7b6e2f58a3074786db
Dmitry Morozov <9984398@gmail.com>
2023-03-04 22:03:15 +0300

    --> File Concatenation
    Done
    
    Tests: None

--- Lesson7/FileConcatenation/Program.cs
+++ Lesson7/FileConcatenation/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿
 Console.WriteLine("FileConcatenation");
+
+static async void FileConcat(string path)
+{
+    if (!Directory.Exists(path))
+    {
+        Console.WriteLine("Directory does not exist.");
+        return;
+    }
+
+    string[] files = Directory.GetFiles(path, "*.txt");
+
+    if (File.Exists("result.txt"))
+    {
+        File.Delete("result.txt");
+    }
+
+    await using StreamWriter sw = File.CreateText("result.txt");
+
+    foreach (string file in files)
+    {
+        using StreamReader sr = File.OpenText(file);
+
+        string? s = "";
+
+        while ((s = await sr.ReadLineAsync()) != null)
+        {
+            await sw.WriteLineAsync(s);
+        }
+    }
+}
+
+// Concatenate all .txt files in the given directory
+FileConcat("../../../Demo");
